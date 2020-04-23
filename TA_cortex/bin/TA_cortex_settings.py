5997669256e0af999002844d9f08ddc915940c24
LmR <letmer00t@gmail.com>
2020-04-23 15:50:30 +0200

    Ignore proxy settings for now

--- TA_cortex/bin/TA_cortex_settings.py
+++ TA_cortex/bin/TA_cortex_settings.py
@@ -47,70 +47,70 @@ model_cortex = RestModel(fields_cortex, name='cortex')
 
 
 
-fields_proxy = [
-    field.RestField(
-        'proxy_enabled',
-        required=False,
-        encrypted=False,
-        default=None,
-        validator=None
-    ), 
-    field.RestField(
-        'proxy_type',
-        required=False,
-        encrypted=False,
-        default='http',
-        validator=None
-    ), 
-    field.RestField(
-        'proxy_url',
-        required=False,
-        encrypted=False,
-        default=None,
-        validator=validator.String(
-            max_len=4096, 
-            min_len=0, 
-        )
-    ), 
-    field.RestField(
-        'proxy_port',
-        required=False,
-        encrypted=False,
-        default=None,
-        validator=validator.Number(
-            max_val=65535, 
-            min_val=1, 
-        )
-    ), 
-    field.RestField(
-        'proxy_username',
-        required=False,
-        encrypted=False,
-        default=None,
-        validator=validator.String(
-            max_len=50, 
-            min_len=0, 
-        )
-    ), 
-    field.RestField(
-        'proxy_password',
-        required=False,
-        encrypted=True,
-        default=None,
-        validator=validator.String(
-            max_len=8192, 
-            min_len=0, 
-        )
-    ), 
-    field.RestField(
-        'proxy_rdns',
-        required=False,
-        encrypted=False,
-        default=None,
-        validator=None
-    )
-]
-model_proxy = RestModel(fields_proxy, name='proxy')
+#fields_proxy = [
+#    field.RestField(
+#        'proxy_enabled',
+#        required=False,
+#        encrypted=False,
+#        default=None,
+#        validator=None
+#    ), 
+#    field.RestField(
+#        'proxy_type',
+#        required=False,
+#        encrypted=False,
+#        default='http',
+#        validator=None
+#    ), 
+#    field.RestField(
+#        'proxy_url',
+#        required=False,
+#        encrypted=False,
+#        default=None,
+#        validator=validator.String(
+#            max_len=4096, 
+#            min_len=0, 
+#        )
+#    ), 
+#    field.RestField(
+#        'proxy_port',
+#        required=False,
+#        encrypted=False,
+#        default=None,
+#        validator=validator.Number(
+#            max_val=65535, 
+#            min_val=1, 
+#        )
+#    ), 
+#    field.RestField(
+#        'proxy_username',
+#        required=False,
+#        encrypted=False,
+#        default=None,
+#        validator=validator.String(
+#            max_len=50, 
+#            min_len=0, 
+#        )
+#    ), 
+#    field.RestField(
+#        'proxy_password',
+#        required=False,
+#        encrypted=True,
+#        default=None,
+#        validator=validator.String(
+#            max_len=8192, 
+#            min_len=0, 
+#        )
+#    ), 
+#    field.RestField(
+#        'proxy_rdns',
+#        required=False,
+#        encrypted=False,
+#        default=None,
+#        validator=None
+#    )
+#]
+#model_proxy = RestModel(fields_proxy, name='proxy')
 
 
 fields_logging = [
@@ -131,7 +131,7 @@ endpoint = MultipleModel(
     'ta_cortex_settings',
     models=[
         model_cortex,
-        model_proxy,
+#        model_proxy,
         model_logging
     ],
 )
